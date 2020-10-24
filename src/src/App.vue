9b6489f9076dff49738f9abadf3d5e05b490e4cb
Diego <diego@soho.cl>
2020-10-24 12:45:43 -0500

    initial

---
+++ src/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <b-sidebar id="sidebar-footer" aria-label="Sidebar with custom footer" no-header shadow visible>
+      <template #footer="{  }">
+       <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
+        <strong class="mr-auto">Footer</strong>
+        <!-- <b-button size="sm" @click="hide">Close</b-button> -->
+       </div>
+      </template>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
